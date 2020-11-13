7e48ef9012f4064ce4d3220d51952b6582a2b5f1
Omoleye Oluseye <omoleye.oluseye@levelten.energy>
2020-11-13 23:04:31 +0100

    fix jumbotron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../views/header/header.component';
+import { JumbotronComponent } from '../views/jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent
+    AppComponent, HeaderComponent, JumbotronComponent
   ],
   imports: [
     BrowserModule,
