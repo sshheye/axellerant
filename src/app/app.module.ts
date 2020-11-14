8447a21c7536b919e1ac4f8f399cd54aaad52f8f
Omoleye Oluseye <omoleye.oluseye@levelten.energy>
2020-11-14 18:26:28 +0100

    include testimonial, footer and locations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../views/header/header.component';
 import { JumbotronComponent } from '../views/jumbotron/jumbotron.component';
+import { LocationsComponent } from '../views/locations/locations.component';
+import { TestimonialsComponent } from '../views/testimonials/testimonials.component';
+import { ContactUsComponent } from '../views/contact-us/contact-us.component';
+import { FooterComponent } from '../views/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, JumbotronComponent
+    AppComponent, HeaderComponent, JumbotronComponent, LocationsComponent,
+    TestimonialsComponent, ContactUsComponent, FooterComponent
   ],
   imports: [
     BrowserModule,
